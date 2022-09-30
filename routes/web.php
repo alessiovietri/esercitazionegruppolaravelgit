5fd7c64edf762657b67c8d0db878ec618313eb0b
LorenzoBertoni <lorenzo.berto99@hotmail.it>
2022-09-30 16:53:39 +0200

    Recuperate le info dal db e stampate nella vista products.

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Product;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    $products = Product::all();
+
+    return view('products', ['products' => $products]);
+})->name('products');
