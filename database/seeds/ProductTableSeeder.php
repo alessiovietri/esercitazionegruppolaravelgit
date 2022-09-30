be704a1d152d702f55dd876d0ddc92ab5750d708
Matteo Susanna <matteo2784@gmail.com>
2022-09-30 16:36:07 +0200

    aggiunta model e seeder

---
+++ database/seeds/ProductTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Product;
+
+class ProductTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = \Faker\Factory::create('it_IT');
+
+        for($i = 0; $i < 10; $i++){
+            $newFaker = new Product();
+
+            $newFaker->name = $faker->word();
+            $newFaker->slug = $faker->word();
+            $newFaker->price = $faker->randomFloat(2,1,100);
+
+            $newFaker->save();
+        }
+    }
+}
