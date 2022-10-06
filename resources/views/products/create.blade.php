08bd4bcaa0a7d812c054d32754af7389a3be4e6d
fathergasc <quaglioniandrea@gmail.com>
2022-10-06 17:12:12 +0200

    add: store

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -3,17 +3,28 @@
 @section('title', 'Aggiungi prodotto')
 
 @section('content')
+
+    @if ($errors->any())
+        <div class="alert alert-warning">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
     <form action="{{route('products.store')}}" method="POST">
         @csrf
 
         <label for="name">Name</label>
-        <input type="text" name="name" id="name">
+        <input type="text" name="name" id="name" required value="{{old('name')}}">
 
         <label for="slug">Slug</label>
-        <input type="text" name="slug" id="slug">
+        <input type="text" name="slug" id="slug" required value="{{old('slug')}}">
 
         <label for="price">Price</label>
-        <input type="number" name="price" id="price">
+        <input type="number" name="price" id="price" value="{{old('price')}}">
 
         <button type="submit">Submit</button>
 
