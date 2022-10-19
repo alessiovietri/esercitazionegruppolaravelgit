77855a2fc73ec6120c088bc4ee4fe74f93d77133
AlexBosio95 <alex.bosio1995@gmail.com>
2022-10-19 17:00:28 +0200

    feat: Api

---
+++ app/Http/Controllers/Api/ProductController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Product;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $product = Product::with(['category', 'orders'])->paginate(2);
+
+        return response()->json([
+            'success' => true,
+            'results' => $product
+        ]);
+    }
+
+
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $product = Product::where('id', $id)->with(['category', 'orders'])->first();
+
+        return response()->json([
+            'success' => true,
+            'results' => $product
+        ]);
+    }
+
+    
+}
